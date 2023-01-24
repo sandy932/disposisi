97e5c34b47b4efea0174ce896f6272f84cc46939
sandy932 <sandymuaz76@gmail.com>
2023-01-24 19:40:24 +0800

    Menambahkan Struktur folder dan file skretaris

---
+++ application/views/template_user/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Sistem Informasi Point</title>
+        <link href="<?= base_url ('assets/css/styles.css')?>" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
