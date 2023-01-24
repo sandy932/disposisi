d5e517dd3ef91e4dd01cd4bb2e4097ac00a43912
sandy932 <sandymuaz76@gmail.com>
2023-01-24 20:30:18 +0800

    perbaikan kode

--- application/views/template_surat/index.php
+++ application/views/template_surat/index.php
@@ -1,28 +1,59 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Template_model extends CI_Model
-{
-    protected $_table ='tb_template_surat';
-    protected $primary = 'id';
-    public function getAll()
-    {
-    return $this->db->where('is_active',1)->get($this->_table)-
-    >result();
-    }
-
-    public function save(){
-    $data = [
-            'nama' => $this->input->post('no_surat'),
-            'tujuan_surat' => $this->input->post('tgl_surat'),
-            'tgl_kirim' => $this->input->post('surat_from'),
-            'perihal' => $this->input->post('surat_to'),
-            'keterangan' => $this->input->post('tgl_terima'),
-            'is_active' => '1',
-            ];
-    $this->db->insert($this->_table,$data);
-    }
-    
-    public function getById($id)
-    {
-    return $this->db->get_where($this->_table, ["id" => $id])->row();
-    }
-}
+<main>
+    <div class="container-fluid">
+        <h1 class="mt-4"></h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="<?php echo site_url('surat_ajuan') ?>">Surat</a></li>
+                <li class="breadcrumb-item active"><?php echo $title ?></li>
+            </ol>
+        <div class="card mb-4">
+            <div class="card-header">
+                <a href="<?php echo site_url('surat_ajuan/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+            </div>
+            <?php if ($this->session->flashdata('success')): ?>
+            <div class="alert alert-success" role="alert">
+            <?php echo $this->session->flashdata('success'); ?>
+            </div>
+            <?php endif; ?>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="tabelsurat" width="100%" cellspacing="0">
+                        <thead class="table-primary">
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama Pengirim</th>
+                            <th>Perihal</th>
+                            <th>Tujuan Surat</th>
+                            <th>Tanggal Kirim</th>
+                            <th>Keterangan</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $no =1;
+                        foreach ($template as $suratdata) {
+                        echo "<tr>
+                                <td>$no</td>
+                                <td>$suratdata->nama</td>
+                                <td>$suratdata->perihal</td>
+                                <td>$suratdata->tujuan_surat</td>
+                                <td>$suratdata->tgl_kirim</td>
+                                <td>$suratdata->keterangan</td>
+                                <td>
+                                <div>
+                                <a href=".base_url('templatesurat/getedit/' .$suratdata->id)." class='btn btn-sm btn-info'><i class='fas fa-edit'></i>Edit</a>
+                                <a href=".base_url('templatesurat/delete/' .$suratdata->id)." class='btn btn-sm btn-danger' onclick='return confirm(\"Ingin mengapus datauser ini?\");'><i class='fas fa-trash'></i> Hapus</a>
+                                <a href=".base_url('templatesurat/surat_ajuan/' . $suratdata->id)." class='btn btn-sm btn-info'><i class='fas fa-download'></i> Unduh</a>
+                                </div>
+                                </td>
+                                </tr>";
+                            $no++;
+                        } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    <div style="height: 100vh"></div>
+</div>
+</main
